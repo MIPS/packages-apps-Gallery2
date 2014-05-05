c39ebe536a3b6bfaed6859ab66140640d9fde2b1
Raghu Gandham <raghu.gandham@imgtec.com>
2014-05-05 14:57:06 -0700

    Use the environment variable USE_GMS_LIBS to rename the library and avoid conflict with the library in GMS repo
    Change-Id: Ic96fd3db0747b41cb7dad97b1ff2c01544e25762

--- jni/Android.mk
+++ jni/Android.mk
@@ -11,8 +11,11 @@ LOCAL_SDK_VERSION := 9
 LOCAL_LDFLAGS :=  -llog -lEGL
 
 LOCAL_MODULE_TAGS := optional
-
-LOCAL_MODULE := libjni_eglfence
+ifeq ($(USE_GMS_LIBS),true)
+	LOCAL_MODULE := libjni_eglfence_old
+else
+	LOCAL_MODULE := libjni_eglfence
+endif
 
 
 include $(BUILD_SHARED_LIBRARY)
@@ -24,7 +27,11 @@ include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cc
 LOCAL_LDFLAGS	:= -llog -ljnigraphics
 LOCAL_SDK_VERSION := 9
-LOCAL_MODULE    := libjni_filtershow_filters
+ifeq ($(USE_GMS_LIBS),true)
+	LOCAL_MODULE    := libjni_filtershow_filters_old
+else
+	LOCAL_MODULE    := libjni_filtershow_filters
+endif
 LOCAL_SRC_FILES := filters/gradient.c \
                    filters/saturated.c \
                    filters/exposure.c \
